41e1cb46aa4290d168cc14662ff6e741b7caab46
bb118314 <roflwhy@gmail.com>
2018-09-18 00:01:59 -0500

    Update and rename MetricConversion2.java to MetricConversion.java

--- Chapter3/src/Excersises/MetricConversion.java
+++ Chapter3/src/Excersises/MetricConversion.java
@@ -1,6 +1,6 @@
 package Excersises;
 import java.util.Scanner;
-public class MetricConversion2 {
+public class MetricConversion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
